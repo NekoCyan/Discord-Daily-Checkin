2c02cf9fb15de49b01dd1776675b52775fab65f6
NekoCyan <cyanhijirikawa88@gmail.com>
2026-05-04 11:02:55 +0700

    fix: Separate class declarations from default exports
    Define AutoCompleteInteraction and CommandInteraction classes first and export them with a separate default export statement at the end of each file. This is a non-functional style change to standardize export syntax across interaction utilities and improve consistency for tooling/transpilation.

--- src/utilities/interaction/autocomplete.interaction.ts
+++ src/utilities/interaction/autocomplete.interaction.ts
@@ -2,8 +2,10 @@ import { AutocompleteInteraction } from 'discord.js';
 import BotClient from '../../BotClient.js';
 import BaseInteraction from './_baseInteraction.js';
 
-export default class AutoCompleteInteraction extends BaseInteraction<AutocompleteInteraction> {
+class AutoCompleteInteraction extends BaseInteraction<AutocompleteInteraction> {
   constructor(client: BotClient, interaction: AutocompleteInteraction) {
     super(client, interaction);
   }
 }
+
+export default AutoCompleteInteraction;
--- src/utilities/interaction/command.interaction.ts
+++ src/utilities/interaction/command.interaction.ts
@@ -2,8 +2,10 @@ import { ChatInputCommandInteraction } from 'discord.js';
 import BotClient from '../../BotClient.js';
 import BaseInteraction from './_baseInteraction.js';
 
-export default class CommandInteraction extends BaseInteraction<ChatInputCommandInteraction> {
+class CommandInteraction extends BaseInteraction<ChatInputCommandInteraction> {
   constructor(client: BotClient, interaction: ChatInputCommandInteraction) {
     super(client, interaction);
   }
 }
+
+export default CommandInteraction;
